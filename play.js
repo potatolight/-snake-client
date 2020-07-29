3a34dfee60828ea819fe8ae47a04436da82df8b9
potatolight <shuguangbai@gmail.com>
2020-07-29 21:54:15 +0000

    addcontrolc

--- play.js
+++ play.js
@@ -4,6 +4,22 @@
  */
 
 const { connect } = require('./client')
-console.log('Connecting ...');
- connect();
 
+console.log('Connecting ...');
+connect();
+
+ const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+ 
+  const handleUserInput = function (data) {
+    if (data === '\u0003') {
+      process.exit();
+    }
+  }
+  stdin.on("data", handleUserInput)
+  return stdin;
+}
+setupInput()
