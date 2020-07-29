cbc04aec4909a942114df44fecb0780edcde84e1
potatolight <shuguangbai@gmail.com>
2020-07-29 23:40:24 +0000

    add string aftern name

--- client.js
+++ client.js
@@ -7,22 +7,16 @@ const connect = function() {
     host: "135.23.222.131",
     port: 50541
   });
-
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
- 
-   conn.on('data', (data) => {
+    conn.setEncoding('utf8'); 
+    conn.on('data', (data) => {
     console.log('Server says: ', data);
    });
- 
-  conn.on('connect', () => {
+    conn.on('connect', () => {
     console.log("Successfully connected to game server")
     conn.write("Name: bsg")
     setupInput(conn)
-    // conn.write("Move: up")
-    // conn.write("Move: down")
-    // conn.write("Move: left")
-    // conn.write("Move: right")
+    conn.write('Say: ggg')
   })
 
   return conn;
