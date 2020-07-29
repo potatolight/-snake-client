11b9fee7c4bdb1ce7072ae1d9019317cbc8a149b
potatolight <shuguangbai@gmail.com>
2020-07-29 20:01:47 +0000

    add play and client

---
+++ client.js
@@ -0,0 +1,19 @@
+const net = require('net');
+const stdin = process.stdin;
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: "135.23.222.131",
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  return conn;
+}
+module.exports = {connect};
