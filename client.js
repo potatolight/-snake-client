68589b7349a2921bca35820aaa6db95d2ea14388
potatolight <shuguangbai@gmail.com>
2020-07-30 00:06:11 +0000

    add constants and change some in the client

--- client.js
+++ client.js
@@ -1,11 +1,13 @@
 const net = require('net');
+const { IP, PORT } = require('./constants');
 const {setupInput} = require('./input')
 const stdin = process.stdin;
 
 const connect = function() {
   const conn = net.createConnection({ 
-    host: "135.23.222.131",
-    port: 50541
+    host: IP,
+    port: PORT
+    
   });
   // interpret incoming data as text
     conn.setEncoding('utf8'); 
